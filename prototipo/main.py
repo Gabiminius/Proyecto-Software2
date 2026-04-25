45df9bfea14770b50a4eeab286bc87d1c7a8a26e
LordFedie <162379039+LordFedie@users.noreply.github.com>
2026-04-25 15:40:37 -0400

    proto

---
+++ prototipo/main.py
@@ -0,0 +1,6 @@
+from aplicacion_web import AplicacionWeb
+
+if __name__ == "__main__":
+    print("\n=== INICIANDO SISTEMA DE CONVIVENCIA ESCOLAR ===\n")
+    app = AplicacionWeb()
+    app.ejecutar_demo()
